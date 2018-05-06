9128428ab072e470d1f7b5524022b3437e8c5409
beaulian <gjw.jesus@qq.com>
2018-05-06 12:00:35 +0800

    commit

--- gen_lognormal.cpp
+++ gen_lognormal.cpp
@@ -16,7 +16,7 @@ int main()
 
   std::mt19937 rng;
   rng.seed(std::random_device()());
-  std::lognormal_distribution<double> dist(0.0, 2.0);
+  std::lognormal_distribution<double> dist(0.0, 3.0);
 
   std::set<int> samples;
 
@@ -24,15 +24,10 @@ int main()
     double r = dist(rng);
     if (r > max) continue;
     samples.insert(int(r * scale));
-    if (samples.size() % 1000000 == 0) {
-      std::cerr << "Generated " << samples.size() << std::endl;
-    }
   }
 
   std::vector<int> vec(samples.begin(), samples.end());
   std::sort(vec.begin(), vec.end());
-  std::cerr << "min = " << vec[0] << std::endl;
-  std::cerr << "max = " << vec[vec.size() - 1] << std::endl;
 
   FILE *fout = fopen("lognormal.sorted.19000.txt", "w");
   for (int x : vec) {
